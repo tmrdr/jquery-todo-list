483298618dd1082e438037d96a80a18a305d3596
Tobias Murphy <murphy.tobias@gmail.com>
2016-12-06 09:34:19 -0800

    ugh

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $("#clicker").click(function(){
   console.log("clicked");
     var toAdd = $("input[name=item]").val();
 
-  $("#list").append("<li>" + toAdd + "</li>");
+  $("#list").append("<li>" + toAdd + " " + "<button class='deleteButton'>x</button></li>");
   $("li").addClass("do");
 })
 
@@ -17,6 +17,20 @@ $("ul[id=list]").click(function(){
     $(this).css("color", "red");
     // $(this).remove());
   });
+
+
+
+  // function removeItem() {
+  //   $(this).parent("li").remove()
+  //
+  // }
+  //
+  // $("#container").on("click", ".deleteButton", function(){
+  //   removeItem($(this));
+  // })
+  //
+
+  // <button class='deleteButton'>x</button></li>"
 
 
 
