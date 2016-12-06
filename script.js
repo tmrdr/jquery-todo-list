58363f3bb6e0f29d50e89f36790c7e5e34a5c74d
Tobias Murphy <murphy.tobias@gmail.com>
2016-12-05 23:01:15 -0800

    partial functionality, cant remove items yet

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+console.log("DOM ready");
+
+$("#clicker").click(function(){
+  console.log("clicked");
+    var toAdd = $("input[name=item]").val();
+
+  $("#list").append("<li>" + toAdd + "</li>");
+  $("li").addClass("do");
+})
+
+
+
+
+$(".do").click(function(){
+    console.log("i have been clicked~!");
+    $(this).css("color", "red"());
+    // $(this).remove());
+  });
+
+
+
+
+
+
+//
+// $(".kill").click(function(){
+//   console.log("delted");
+//   $("#list li").eq(0).remove();
+// })
+
+});
